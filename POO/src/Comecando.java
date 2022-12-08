6c3cdd12eab7cbd2b85562ada80fe53fbf0e5a58
Paulo Palmeira <paulopalmeirasp@gmail.com>
2022-12-08 17:49:29 -0300

    Training

---
+++ POO/src/Comecando.java
@@ -0,0 +1,10 @@
+
+public class Comecando {
+
+	public static void main(String[] args) {
+		
+	System.out.println("teste");
+	
+	}
+
+}
--- Testando.java
+++ Testando.java
@@ -1,10 +1,9 @@
-
 public class Testando {
 
 	public static void main(String[] args) {
-
 		
-
+		System.out.println("o System tem que estar com o S maiusculo");
+		
 	}
 
 }
